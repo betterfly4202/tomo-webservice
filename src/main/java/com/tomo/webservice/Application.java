d43579ddcef837c8972d205eb55c9a8214ca571a
이충일 <betterfly4202@gmail.com>
2019-03-20 16:17:57 +0900

    no message

---
+++ src/main/java/com/tomo/webservice/Application.java
@@ -0,0 +1,16 @@
+package com.tomo.webservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by betterfly
+ * Date : 2019.03.20
+ */
+
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
